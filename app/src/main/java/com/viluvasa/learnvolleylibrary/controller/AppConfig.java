d4751abe90690503d09bb885952338bbc6c316ce
Anaz Satria Aji <aji.anaz@gmail.com>
2017-10-18 20:52:54 +0700

    Initial commit

---
+++ app/src/main/java/com/viluvasa/learnvolleylibrary/controller/AppConfig.java
@@ -0,0 +1,14 @@
+package com.viluvasa.learnvolleylibrary.controller;
+
+/**
+ * Created by AJI on 1/23/2016.
+ */
+public class AppConfig {
+
+    public static String HOST = "http://tmp.satukata.id/api/";
+
+    public static String URL_DataMuseum = HOST+"data_museum?api=4ndr01dmu5eum";
+    public static String URL_Maps = "https://maps.googleapis.com/maps/api/directions/json?origin=Brooklyn&destination=Queens&departure_time=1343641500&mode=transit";
+    //public static String APIKEY = "4ndr01dmu5eum";
+
+}
